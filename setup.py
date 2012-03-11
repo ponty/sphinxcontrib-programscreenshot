1baf48af4eded0b21d684c74be8f44bec8b6983d
ponty <ponty@home>
2012-03-11 16:39:56 +0100

    new setup

--- setup.py
+++ setup.py
@@ -1,7 +1,60 @@
-import os
-if os.path.exists("paver-minilib.zip"):
-    import sys
-    sys.path.insert(0, "paver-minilib.zip")
+from setuptools import find_packages, setup
+import os.path
+import sys
 
-import paver.tasks
-paver.tasks.main()
+
+def read_project_version(*py):
+    py = os.path.join(*py)
+    __version__ = None
+    for line in open(py).read().splitlines():
+        if '__version__' in line:
+            exec(line)
+            break
+    return __version__
+
+NAME = 'sphinxcontrib-programscreenshot'
+URL = 'https://github.com/ponty/sphinxcontrib-programscreenshot'
+DESCRIPTION = 'Sphinx extension to include screenshot of programs'
+VERSION = read_project_version('sphinxcontrib' , 'programscreenshot.py')
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+classifiers = [
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    "License :: OSI Approved :: BSD License",
+    "Natural Language :: English",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python",
+    'Environment :: Console',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'Topic :: Documentation',
+    'Topic :: Utilities',
+    ]
+
+install_requires = open("requirements.txt").read().split('\n')
+
+# compatible with distutils of python 2.3+ or later
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=open('README.rst', 'r').read(),
+    classifiers=classifiers,
+    keywords='sphinx screenshot',
+    author='ponty',
+    #author_email='',
+    url=URL,
+    license='BSD',
+    packages=find_packages(exclude=['bootstrap', 'pavement', ]),
+    include_package_data=True,
+    test_suite='nose.collector',
+    zip_safe=False,
+    install_requires=install_requires,
+    namespace_packages=['sphinxcontrib'],
+    **extra
+    )
+
+
